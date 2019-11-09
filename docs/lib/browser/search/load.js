6a7f72e039247dac88bc38055f7abbb72b5bdbc5
moxingwang <xingwang.mo@chinaredstar.com>
2019-11-09 17:37:45 +0800

    init doc

---
+++ docs/lib/browser/search/load.js
@@ -0,0 +1,26 @@
+const lunr = require('lunr');
+const searcher = require('../../nodejs/search/searcher');
+
+module.exports = function load(url) {
+  return fetchIndex(url)
+    .then((data) => {
+      console.log('search load.js');
+      console.log(data.index);
+      return searcher({
+        index: data.index,
+        store: data.store
+      });
+    });
+};
+
+function fetchIndex(url) {
+  return fetch(url || '/lunr.json', { credentials: 'include' })
+    .then(function(res) {
+      return res.json();
+    })
+    .then(function(json) {
+      const index = lunr.Index.load(json.index);
+      const store = json.store;
+      return { index: index, store: store };
+    });
+}
