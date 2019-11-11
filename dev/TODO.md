5b4076fe5fc33181cac61fa23c0a0ceb658ad0b5
xingwang.mo <xingwang.mo@chinaredstar.com>
2019-11-11 11:18:09 +0800

    ADD TODO

--- dev/TODO.md
+++ dev/TODO.md
@@ -1,2 +1,3 @@
-- 待办
-    - 去core  ，瘦身agent, 走其他方式
+- 去core  ，瘦身agent, 走其他方式
+- MockSpy 加break
+- 校验和其他字节码框架有没有冲突，理论上是有的，走visitLocalVariable这种方式操作局部变量表最安全
---
+++ neeza-test/src/main/java/io/github/hotspacode/neeza/test/source/ObjectSource2.java
@@ -0,0 +1,72 @@
+package io.github.hotspacode.neeza.test.source;
+
+import io.github.hotspacode.neeza.deputy.core.MockSpy;
+import io.github.hotspacode.neeza.deputy.dto.MockTransport;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class ObjectSource2 {
+    public static void main(String[] args) {
+        Class<ObjectSource2> objectSource2Class = ObjectSource2.class;
+        for (Method method : objectSource2Class.getMethods()) {
+            int count = method.getParameterCount();
+
+            System.out.println(1);
+        }
+
+    }
+
+
+    public Map createSource0() {
+        List<Object> methodParams = new ArrayList<>();
+
+        MockTransport mockResponse = MockSpy.getMockData(Thread.currentThread().getStackTrace()[1],methodParams);
+        if (mockResponse.isMocked()) {
+            return mockResponse.getObject(mockResponse);
+        }
+
+        String dd = "111";
+
+        System.out.println("我在正常执行...");
+        return null;
+    }
+
+   public Map createSource1(String a,String b ) {
+       List<Object> methodParams = new ArrayList<>();
+       methodParams.add(a);
+       methodParams.add(b);
+
+       MockTransport mockResponse = MockSpy.getMockData(Thread.currentThread().getStackTrace()[1],methodParams);
+       if (mockResponse.isMocked()) {
+           return mockResponse.getObject(mockResponse);
+       }
+
+       String dd = "111";
+
+        System.out.println("我在正常执行...");
+        return null;
+    }
+
+
+    public Map createSource2(String a,String b ,String c,String d ) {
+        List<Object> methodParams = new ArrayList<>();
+        methodParams.add(a);
+        methodParams.add(b);
+        methodParams.add(c);
+        methodParams.add(d);
+
+        MockTransport mockResponse = MockSpy.getMockData(Thread.currentThread().getStackTrace()[1]);
+        if (mockResponse.isMocked()) {
+            return mockResponse.getObject(mockResponse);
+        }
+
+
+        System.out.println("我在正常执行...");
+        return null;
+    }
+
+
+}
