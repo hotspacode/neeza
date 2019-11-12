5dff62143ae9256f58633a964738f75383abd6b6
moxingwang <xingwang.mo@chinaredstar.com>
2019-11-12 15:38:26 +0800

    change SPI serialization package

--- neeza-core/src/main/java/io/github/hotspacode/neeza/core/domain/core/Entity.java
+++ neeza-core/src/main/java/io/github/hotspacode/neeza/core/domain/core/Entity.java
@@ -7,7 +7,7 @@ import java.util.List;
  * 资源
  */
 public class Entity implements Serializable {
-    private String descriptor;//资源描述
+    private String descriptor;//资源描述   io.github.hotspacode.neeza.core.domain.core.createOrder(java,long.String,)
     private String className;
     private String methodName;
     private List<Rule> rules;
