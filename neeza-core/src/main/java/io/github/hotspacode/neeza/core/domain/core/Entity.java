c5fc34e1f8c446425aa94e4c760f9ab0c0324bd0
xingwang.mo <xingwang.mo@chinaredstar.com>
2019-11-12 22:20:30 +0800

    define

--- neeza-core/src/main/java/io/github/hotspacode/neeza/core/domain/core/Entity.java
+++ neeza-core/src/main/java/io/github/hotspacode/neeza/core/domain/core/Entity.java
@@ -10,5 +10,8 @@ public class Entity implements Serializable {
     private String descriptor;//资源描述   io.github.hotspacode.neeza.core.domain.core.createOrder(java,long.String,)
     private String className;
     private String methodName;
+    private List<String> methodParamClassNames;
+    private String returnClassNames;
+
     private List<Rule> rules;
 }
