aca2c4c173d378d66df6ab84ac06b0668c42f4b1
moxingwang <xingwang.mo@chinaredstar.com>
2019-11-12 14:55:46 +0800

    change SPI serialization package

--- neeza-core/src/main/java/io/github/hotspacode/neeza/core/serialization/FastJSONSerialization.java
+++ neeza-core/src/main/java/io/github/hotspacode/neeza/core/serialization/FastJSONSerialization.java
@@ -1,4 +1,4 @@
-package io.github.hotspacode.neeza.core.service.serialization;
+package io.github.hotspacode.neeza.core.serialization;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.parser.ParserConfig;
