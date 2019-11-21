ab7b9c874f5cc7662128a4b4e9ca6d98182311f9
moxingwang <xingwang.mo@chinaredstar.com>
2019-11-21 17:52:52 +0800

    define netty api

--- neeza-transport/neeza-transport-netty-http/src/main/java/io/github/hotspacode/neeza/transport/netty/http/NettyHeartbeatSender.java
+++ neeza-transport/neeza-transport-netty-http/src/main/java/io/github/hotspacode/neeza/transport/netty/http/NettyHeartbeatSender.java
@@ -1,7 +1,9 @@
 package io.github.hotspacode.neeza.transport.netty.http;
 
+import io.github.hotspacode.neeza.deputy.annotation.SpiOrder;
 import io.github.hotspacode.neeza.transport.api.HeartbeatSender;
 
+@SpiOrder(SpiOrder.LOWEST_PRECEDENCE - 100)
 public class NettyHeartbeatSender implements HeartbeatSender {
     @Override
     public boolean sendHeartbeat() throws Exception {
--- neeza-transport/neeza-transport-netty-http/src/main/java/io/github/hotspacode/neeza/transport/netty/http/NettyTransportServerCenter.java
+++ neeza-transport/neeza-transport-netty-http/src/main/java/io/github/hotspacode/neeza/transport/netty/http/NettyTransportServerCenter.java
@@ -1,7 +1,9 @@
 package io.github.hotspacode.neeza.transport.netty.http;
 
+import io.github.hotspacode.neeza.deputy.annotation.SpiOrder;
 import io.github.hotspacode.neeza.transport.api.TransportServerCenter;
 
+@SpiOrder(SpiOrder.LOWEST_PRECEDENCE - 100)
 public class NettyTransportServerCenter implements TransportServerCenter {
 
     @Override
