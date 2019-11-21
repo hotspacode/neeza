da24b3a4098616a2cb46bb35abc8fde9055a58fc
moxingwang <xingwang.mo@chinaredstar.com>
2019-11-21 17:28:16 +0800

    define netty api

--- neeza-deputy/src/main/java/io/github/hotspacode/neeza/deputy/common/SpiLoader.java
+++ neeza-deputy/src/main/java/io/github/hotspacode/neeza/deputy/common/SpiLoader.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 1999-2018 Alibaba Group Holding Ltd.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package io.github.hotspacode.neeza.deputy.common;
 
 import io.github.hotspacode.neeza.deputy.annotation.SpiOrder;
@@ -28,7 +13,6 @@ public final class SpiLoader {
     public static <T> T loadHighestPriorityInstance(Class<T> clazz) {
         try {
             String key = clazz.getName();
-            // Not thread-safe, as it's expected to be resolved in a thread-safe context.
             ServiceLoader<T> serviceLoader = SERVICE_LOADER_MAP.get(key);
             if (serviceLoader == null) {
                 serviceLoader = ServiceLoader.load(clazz, clazz.getClassLoader());
