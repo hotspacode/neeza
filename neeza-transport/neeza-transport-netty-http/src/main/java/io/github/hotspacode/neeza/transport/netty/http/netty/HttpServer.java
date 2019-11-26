a60bde2e43613e4f5510b5a097628a93a0786179
moxingwang <xingwang.mo@chinaredstar.com>
2019-11-26 16:36:33 +0800

    define netty api

--- neeza-transport/neeza-transport-netty-http/src/main/java/io/github/hotspacode/neeza/transport/netty/http/netty/HttpServer.java
+++ neeza-transport/neeza-transport-netty-http/src/main/java/io/github/hotspacode/neeza/transport/netty/http/netty/HttpServer.java
@@ -1,8 +1,44 @@
 package io.github.hotspacode.neeza.transport.netty.http.netty;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
 /**
  * @author moxingwang
  */
 public class HttpServer {
+    private static final Integer DEFAULT_PORT = 8818;
+
+    public void start() throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1); // (1)
+        EventLoopGroup workerGroup = new NioEventLoopGroup(2);
+        try {
+            ServerBootstrap b = new ServerBootstrap(); // (2)
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class) // (3)
+//                    .handler()
+//                    .childHandler()
+                    .option(ChannelOption.SO_BACKLOG, 128)          // (5)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true); // (6)
+
+            // Bind and start to accept incoming connections.
+            ChannelFuture f = b.bind(DEFAULT_PORT).sync(); // (7)
+
+            // Wait until the server socket is closed.
+            // In this example, this does not happen, but you can do that to gracefully
+            // shut down your server.
+            f.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+
+    }
 
 }
