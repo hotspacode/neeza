55b05919b83b2fca6841603957090cee6d348e38
moxingwang <xingwang.mo@chinaredstar.com>
2020-01-14 17:49:39 +0800

    netty transport

--- neeza-transport/neeza-transport-netty-http/src/test/java/io/github/hotspacode/neeza/transport/netty/http/NettyServerTest.java
+++ neeza-transport/neeza-transport-netty-http/src/test/java/io/github/hotspacode/neeza/transport/netty/http/NettyServerTest.java
@@ -3,14 +3,26 @@ package io.github.hotspacode.neeza.transport.netty.http;
 import io.github.hotspacode.neeza.transport.api.init.TransportServerCenterInitHandler;
 import org.junit.Test;
 
+import java.util.concurrent.CountDownLatch;
+
 /**
  * @author moxingwang
  */
 public class NettyServerTest {
-    @Test
-    public void testServer() {
+    public static void main(String[] args) {
         TransportServerCenterInitHandler initHandler = new TransportServerCenterInitHandler();
         initHandler.init();
         System.out.println("netty server init test started !");
+//        Thread.sleep(100000000);
+    }
+
+    @Test
+    public void testServer() throws InterruptedException {
+        TransportServerCenterInitHandler initHandler = new TransportServerCenterInitHandler();
+        initHandler.init();
+        System.out.println("netty server init test started !");
+//        Thread.sleep(100000000);
+        return;
     }
 }
+
