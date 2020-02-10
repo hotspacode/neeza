2d050f5ad46893f7c1ae55babacb63688ac02c9b
moxingwang <xingwang.mo@chinaredstar.com>
2020-02-10 22:25:03 +0800

    add neeza log class

--- neeza-server/neeza-server-standalone/src/main/java/io/github/hotspacode/neeza/server/standalone/service/MockDataService.java
+++ neeza-server/neeza-server-standalone/src/main/java/io/github/hotspacode/neeza/server/standalone/service/MockDataService.java
@@ -8,30 +8,50 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
-import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 public class MockDataService {
     private static final Logger logger = LoggerFactory.getLogger(MockDataService.class);
 
-    private static final Map<String, MockData> cache = new ConcurrentHashMap<>();
+    private static final String PREFIX_IP_PORT = "IPPORT:";
+    private static final String PREFIX_TEMPLATE_DATA = "TEMPLATEDATA:";
 
-    public void saveData(String methodDesc, MockData data) {
+    private static final Map<String, ConcurrentHashMap<String, Object>> cache = new ConcurrentHashMap<>();
+
+    public void saveData(String methodDesc, MockData data, String ip, String port) {
         logger.info("save method desc {},{}", methodDesc, JSON.toJSONString(data));
 
         //todo 校验 daat
 
-        MockData cacheData = cache.get(methodDesc);
+        boolean template = StringUtil.isBlank(ip);
 
-        if (null != cacheData && Objects.equals(cacheData.getBody(), data.getBody())) {
+        ConcurrentHashMap<String, Object> cacheMap = cache.get(methodDesc);
+
+        if (null == cacheMap) {
+
+            MockData cacheMockData = null;
+
+            if (template) {
+
+            }
+
+
             logger.info("no update {},{}", methodDesc, JSON.toJSONString(data));
             return;
+        } else {
+            cacheMap = new ConcurrentHashMap<>();
         }
 
+        if (template) {
+            cacheMap.put(PREFIX_TEMPLATE_DATA, data);
+        } else {
+            cacheMap.put(PREFIX_IP_PORT + ":" + ip + ":" + port, data);
+        }
 
-        cache.put(methodDesc, data);
+        cache.put(methodDesc, cacheMap);
 
 
     }
+
 }
