a12b7f4396342edb491418eb239ca35311ae7d9f
moxingwang <xingwang.mo@chinaredstar.com>
2020-02-10 21:59:15 +0800

    add neeza log class

--- neeza-server/neeza-server-standalone/src/main/java/io/github/hotspacode/neeza/server/standalone/service/ClientManagementService.java
+++ neeza-server/neeza-server-standalone/src/main/java/io/github/hotspacode/neeza/server/standalone/service/ClientManagementService.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
@@ -12,18 +13,23 @@ import java.util.concurrent.ConcurrentHashMap;
 public class ClientManagementService {
     private static final Logger logger = LoggerFactory.getLogger(ClientManagementService.class);
 
-    private static final Map<String, Integer> cache = new ConcurrentHashMap<>();
+    private static final Map<String, Integer> portCache = new ConcurrentHashMap<>();
+    private static final Map<String, List<String>> methodClientCache = new ConcurrentHashMap<>();
 
-    public void loadClient(String ip, Integer port) {
-        Integer cachePort = cache.get(ip);
+    public void loadClient(String ip, Integer port,String methodDesc) {
+        Integer cachePort = portCache.get(ip);
         if (Objects.equals(port, cachePort)) {
             return;
         }
 
-        cache.put(ip, port);
+        portCache.put(ip, port);
     }
 
     public void methodDescDataChangeNotice(){
+
+    }
+
+    class MethodObj{
 
     }
 }
