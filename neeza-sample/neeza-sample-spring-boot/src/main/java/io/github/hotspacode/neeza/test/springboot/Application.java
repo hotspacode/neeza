95ed10b07cb253bde1f7b08f33b1be20354c5217
moxingwang <xingwang.mo@chinaredstar.com>
2020-06-04 17:21:47 +0800

    add

--- neeza-sample/neeza-sample-spring-boot/src/main/java/io/github/hotspacode/neeza/test/springboot/Application.java
+++ neeza-sample/neeza-sample-spring-boot/src/main/java/io/github/hotspacode/neeza/test/springboot/Application.java
@@ -1,5 +1,7 @@
 package io.github.hotspacode.neeza.test.springboot;
 
+import io.github.hotspacode.neeza.base.util.NeezaConstant;
+import io.github.hotspacode.neeza.spy.NeezaServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+		NeezaServer.start("localhost", NeezaConstant.DEFAULT_SERVER_PORT,"io/github/hotspacode/neeza/test");
 		SpringApplication.run(Application.class, args);
 	}
 
--- neeza-sample/neeza-sample-spring-boot/src/main/java/io/github/hotspacode/neeza/test/springboot/IndexController.java
+++ neeza-sample/neeza-sample-spring-boot/src/main/java/io/github/hotspacode/neeza/test/springboot/IndexController.java
@@ -1,9 +1,11 @@
 package io.github.hotspacode.neeza.test.springboot;
 
+import io.github.hotspacode.neeza.base.annotation.NeezaMock;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("index")
+@NeezaMock
 public class IndexController {
     @GetMapping("/name")
     public String indexName() {
