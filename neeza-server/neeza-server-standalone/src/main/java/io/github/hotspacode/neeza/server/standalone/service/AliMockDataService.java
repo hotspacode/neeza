58781a3a90f54470229371dd214452731af904d5
xingwang.mo <xingwang.mo@chinaredstar.com>
2020-06-06 22:06:32 +0800

    commit

--- neeza-server/neeza-server-standalone/src/main/java/io/github/hotspacode/neeza/server/standalone/service/AliMockDataService.java
+++ neeza-server/neeza-server-standalone/src/main/java/io/github/hotspacode/neeza/server/standalone/service/AliMockDataService.java
@@ -4,7 +4,6 @@ package io.github.hotspacode.neeza.server.standalone.service;
 import io.github.hotspacode.neeza.base.dto.MockData;
 import io.github.hotspacode.neeza.server.standalone.dao.ApiMockDataRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.annotation.AccessType;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,7 +12,7 @@ public class AliMockDataService {
     @Autowired
     private ApiMockDataRepository apiMockDataRepository;
 
-    public MockData getData(String methodDesc){
+    public MockData getData(String methodDesc) {
         MockData mockDataDTO = new MockData();
         mockDataDTO.setType(MockData.Type.NONE);
 
