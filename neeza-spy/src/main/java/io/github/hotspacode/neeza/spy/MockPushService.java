0af3ecef35ab950c2120f93dc7e382b2be0a0ab0
xingwang.mo <xingwang.mo@chinaredstar.com>
2020-06-07 17:39:17 +0800

    commit

--- neeza-spy/src/main/java/io/github/hotspacode/neeza/spy/MockPushService.java
+++ neeza-spy/src/main/java/io/github/hotspacode/neeza/spy/MockPushService.java
@@ -3,14 +3,31 @@ package io.github.hotspacode.neeza.spy;
 import io.github.hotspacode.neeza.base.api.IMockPushSpyService;
 import io.github.hotspacode.neeza.base.dto.PushTransportData;
 import io.github.hotspacode.neeza.base.util.MethodInvokeUtil;
+import io.github.hotspacode.neeza.core.serialization.FastJSONSerialization;
 
 public class MockPushService {
 
+    private FastJSONSerialization neezaSerialization = new FastJSONSerialization();
     public static IMockPushSpyService mockPushSpyService;
 
-    public Object push(PushTransportData transportData) {
+    public Object push(PushTransportData transportData) throws ClassNotFoundException {
         Object service = mockPushSpyService.getService(transportData.getServiceName());
-        Object result = MethodInvokeUtil.methodInvoke(service, transportData.getMethodName(), null);
+        Object[] params = null;
+        if (null != transportData.getParamList() && !transportData.getParamList().isEmpty()) {
+            params = new Object[transportData.getParamList().size()];
+        }
+
+        for (int i = 0; i < transportData.getParamList().size(); i++) {
+            if (null != transportData.getParamList().get(i)) {
+                Class targetClass = Class.forName(transportData.getParamList().get(i).getClassName());
+                Object deserialize = neezaSerialization.deserialize(transportData.getParamList().get(i).getContent().getBytes(), targetClass);
+                params[i] = deserialize;
+            } else {
+                params[i] = null;
+            }
+        }
+
+        Object result = MethodInvokeUtil.methodInvoke(service, transportData.getMethodName(), params);
 
         return result;
     }
