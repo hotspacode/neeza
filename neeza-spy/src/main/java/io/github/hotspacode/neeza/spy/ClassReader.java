57589ad87376ba4ea80906ca51ea56cf0e353dda
moxingwang <xingwang.mo@chinaredstar.com>
2020-06-19 18:09:30 +0800

    add

--- neeza-spy/src/main/java/io/github/hotspacode/neeza/spy/ClassReader.java
+++ neeza-spy/src/main/java/io/github/hotspacode/neeza/spy/ClassReader.java
@@ -1,20 +1,37 @@
 package io.github.hotspacode.neeza.spy;
 
+import io.github.hotspacode.neeza.core.domain.core.Entity;
+import io.github.hotspacode.neeza.core.domain.core.clazz.NeezaClazz;
 import sun.reflect.generics.reflectiveObjects.ParameterizedTypeImpl;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
-import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class ClassReader {
 
-    public static void readMethod(Class service){
+    public static void readMethod(Class service) {
         Method[] declaredMethods = service.getDeclaredMethods();
         if (null != declaredMethods && declaredMethods.length > 0) {
             for (Method declaredMethod : declaredMethods) {
                 //解析返回对象
+                //返回对象
+                {
+                    Type genericReturnType = declaredMethod.getGenericReturnType();
+                    if (genericReturnType instanceof ParameterizedTypeImpl) {
+                        ParameterizedTypeImpl typeImpl = (ParameterizedTypeImpl) genericReturnType;
+                        Type[] actualTypeArguments = typeImpl.getActualTypeArguments();
+                        Class<?> rawType = typeImpl.getRawType();
+                        Type ownerType = typeImpl.getOwnerType();
+
+                        Class<? extends Type> aClass = genericReturnType.getClass();
+
+                        System.out.println(1);
+                    }
+                }
 
                 //解析参数列表对象
 
@@ -23,54 +40,30 @@ public class ClassReader {
 
     }
 
-    public static void readClass(Class clazz)  {
-        String className = clazz.getName();
+    public static NeezaClazz readClass(Class clazz) {
+        NeezaClazz neezaClazz = new NeezaClazz();
+        neezaClazz.setType(clazz.getTypeName());
 
-        try {
-            Method[] declaredMethods = clazz.getDeclaredMethods();
-            if (null != declaredMethods && declaredMethods.length > 0) {
-                for (Method declaredMethod : declaredMethods) {
-                    String name = declaredMethod.getName();
-                    Class<?> returnType = declaredMethod.getReturnType();
-
-                    //参数对象
-                    {
-                        Class<?>[] parameterTypes = declaredMethod.getParameterTypes();
-                        if (parameterTypes != null && parameterTypes.length > 0) {
-                            for (Class<?> parameterType : parameterTypes) {
-
-                            }
-                        }
-
-                    }
-
-
-                    //返回对象
-                    {
-                        Type genericReturnType = declaredMethod.getGenericReturnType();
-                        if (genericReturnType instanceof ParameterizedTypeImpl) {
-                            ParameterizedTypeImpl typeImpl = (ParameterizedTypeImpl) genericReturnType;
-                            Type[] actualTypeArguments = typeImpl.getActualTypeArguments();
-                            Class<?> rawType = typeImpl.getRawType();
-                            Type ownerType = typeImpl.getOwnerType();
-
-                            Class<? extends Type> aClass = genericReturnType.getClass();
-
-                            System.out.println(1);
-                        }
-                    }
-
-
-
-                    System.out.println(1);
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+        Field[] declaredFields = clazz.getDeclaredFields();
+        if (null == declaredFields || declaredFields.length == 0) {
+            return neezaClazz;
         }
+
+        List<NeezaClazz> filds = new ArrayList<>();
+        for (Field declaredField : declaredFields) {
+            if (declaredField.getClass().isPrimitive()) {
+                NeezaClazz clazzFild = new NeezaClazz();
+                clazzFild.setType(declaredField.getClass().getTypeName());
+                filds.add(clazzFild);
+            }else {
+                filds.add(readClass(declaredField.getClass()));
+            }
+        }
+        neezaClazz.setFields(filds);
 
 
         System.out.println(1);
+        return neezaClazz;
     }
 
     public static void main(String[] args) {
@@ -80,6 +73,7 @@ public class ClassReader {
 }
 
 class A<T> {
+    private String a;
     public void t1(String a, String[] b) {
 
     }
@@ -88,7 +82,7 @@ class A<T> {
         return null;
     }
 
-    public T t3(){
+    public T t3() {
         return null;
     }
 }
