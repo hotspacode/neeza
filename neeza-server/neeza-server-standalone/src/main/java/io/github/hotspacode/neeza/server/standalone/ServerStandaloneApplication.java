51daa157b6ef7171859c52b8c7e7ebb58cdf9a9f
xingwang.mo <xingwang.mo@chinaredstar.com>
2020-06-21 20:30:11 +0800

    commit

--- neeza-server/neeza-server-standalone/src/main/java/io/github/hotspacode/neeza/server/standalone/ServerStandaloneApplication.java
+++ neeza-server/neeza-server-standalone/src/main/java/io/github/hotspacode/neeza/server/standalone/ServerStandaloneApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 @EnableJpaRepositories(basePackages = "io.github.hotspacode.neeza.server.standalone.dao")
 @EntityScan(basePackages = "io.github.hotspacode.neeza.server.standalone.model")
 public class ServerStandaloneApplication {
