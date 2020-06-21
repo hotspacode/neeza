e70371278a34a9d2c9432935fae47b37f853ef29
xingwang.mo <xingwang.mo@chinaredstar.com>
2020-06-21 21:51:47 +0800

    commit

--- neeza-server/neeza-server-standalone/src/main/java/io/github/hotspacode/neeza/server/standalone/controller/IndexController.java
+++ neeza-server/neeza-server-standalone/src/main/java/io/github/hotspacode/neeza/server/standalone/controller/IndexController.java
@@ -1,21 +1,61 @@
 package io.github.hotspacode.neeza.server.standalone.controller;
 
+import io.github.hotspacode.neeza.base.dto.Result;
+import io.github.hotspacode.neeza.server.standalone.service.MockDataService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 @RequestMapping
 public class IndexController {
+    @Autowired
+    private MockDataService mockDataService;
+
     @RequestMapping("/index")
     public String index() {
         return "index";
     }
 
+    /**
+     * 获取所有app
+     *
+     * @return
+     */
     @RequestMapping("/app/list")
     @ResponseBody
-    public String appList() {
+    public Result<List<String>> appList() {
+        List<String> appList = mockDataService.getAppList();
+        return Result.success(appList);
+    }
 
-        return "index";
+    /**
+     * 获取app下所有service
+     *
+     * @return
+     */
+    @RequestMapping("/service/list/{appName}")
+    @ResponseBody
+    public Result<List<String>> serviceList(@PathVariable(value = "appName") String appName) {
+
+
+        return Result.success();
+    }
+
+    /**
+     * 获取服务明细
+     *
+     * @return
+     */
+    @RequestMapping("/method/detail/{appName}")
+    @ResponseBody
+    public Result<List<String>> methodDetail(@PathVariable(value = "methodDesc") String methodDesc) {
+
+
+        return Result.success();
     }
 }
